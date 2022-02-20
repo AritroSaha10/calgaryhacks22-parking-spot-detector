89b42c0fbe26330a919099ae127d4b0641863203
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2022-02-20 12:54:54 -0500

    Create detect.py

---
+++ detect.py
@@ -0,0 +1,176 @@
+# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Main script to run the object detection routine."""
+import argparse
+import sys
+import time
+
+import cv2
+from object_detector import ObjectDetector
+from object_detector import ObjectDetectorOptions
+import utils
+
+AREA_OF_SPOT = 250
+
+def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
+        enable_edgetpu: bool) -> None:
+  """Continuously run inference on images acquired from the camera.
+
+  Args:
+    model: Name of the TFLite object detection model.
+    camera_id: The camera id to be passed to OpenCV.
+    width: The width of the frame captured from the camera.
+    height: The height of the frame captured from the camera.
+    num_threads: The number of CPU threads to run the model.
+    enable_edgetpu: True/False whether the model is a EdgeTPU model.
+  """
+
+  # Variables to calculate FPS
+  counter, fps = 0, 0
+  start_time = time.time()
+
+  # Start capturing video input from the camera
+  cap = cv2.VideoCapture(camera_id)
+  cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+  cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+  # Visualization parameters
+  row_size = 20  # pixels
+  left_margin = 24  # pixels
+  text_color = (0, 0, 255)  # red
+  font_size = 3
+  font_thickness = 4
+  fps_avg_frame_count = 10
+
+  # Initialize the object detection model
+  options = ObjectDetectorOptions(
+      num_threads=num_threads,
+      score_threshold=0.3,
+      max_results=3,
+      enable_edgetpu=enable_edgetpu)
+  detector = ObjectDetector(model_path=model, options=options)
+
+  # Continuously capture images from the camera and run inference
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      sys.exit(
+          'ERROR: Unable to read from webcam. Please verify your webcam settings.'
+      )
+
+    counter += 1
+    image = cv2.flip(image, 1)
+
+    # Run object detection estimation using the model.
+    detections = detector.detect(image)
+    print(detections)
+      
+    # Draw keypoints and edges on input image
+    # image = utils.visualize(image, detections)
+
+    # Calculate the FPS
+    if counter % fps_avg_frame_count == 0:
+      end_time = time.time()
+      fps = fps_avg_frame_count / (end_time - start_time)
+      start_time = time.time()
+
+    # Show the FPS
+    fps_text = 'FPS = {:.1f}'.format(fps)
+    text_location = (left_margin, row_size)
+    # cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+    #             font_size, text_color, font_thickness)
+
+    carInSpot = False
+
+    # Check whether there's a car in the frame
+    for detection in detections:
+      if detection.categories[0].label == "car" or detection.categories[0].label == "truck":
+        print("Car found! Is it big enough?")
+
+        # Get area of detection zone and check if its larger than a certain amount of pixels^2
+        if (detection.bounding_box.right - detection.bounding_box.left) * (detection.bounding_box.bottom - detection.bounding_box.top) > AREA_OF_SPOT**2:
+          print("Yes!")
+          carInSpot = True
+        else:
+          print("No :(")
+        break
+    else:
+      print("No car :(")
+    
+    print()
+
+    if carInSpot:
+      # Show the FPS
+      car_text = "Car in spot!"
+      text_location = (left_margin, row_size * 4)
+      cv2.putText(image, car_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                  font_size, text_color, font_thickness)
+    else:
+      # Show the FPS
+      car_text = "No car in spot"
+      text_location = (left_margin, row_size * 4)
+      cv2.putText(image, car_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                  font_size, text_color, font_thickness)
+
+    # Stop the program if the ESC key is pressed.
+    if cv2.waitKey(1) == 27:
+      break
+    cv2.imshow('object_detector', image)
+
+  cap.release()
+  cv2.destroyAllWindows()
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument(
+      '--model',
+      help='Path of the object detection model.',
+      required=False,
+      default='efficientdet_lite0.tflite')
+  parser.add_argument(
+      '--cameraId', help='Id of camera.', required=False, type=int, default=0)
+  parser.add_argument(
+      '--frameWidth',
+      help='Width of frame to capture from camera.',
+      required=False,
+      type=int,
+      default=640)
+  parser.add_argument(
+      '--frameHeight',
+      help='Height of frame to capture from camera.',
+      required=False,
+      type=int,
+      default=480)
+  parser.add_argument(
+      '--numThreads',
+      help='Number of CPU threads to run the model.',
+      required=False,
+      type=int,
+      default=4)
+  parser.add_argument(
+      '--enableEdgeTPU',
+      help='Whether to run the model on EdgeTPU.',
+      action='store_true',
+      required=False,
+      default=False)
+  args = parser.parse_args()
+
+  run(args.model, int(args.cameraId), args.frameWidth, args.frameHeight,
+      int(args.numThreads), bool(args.enableEdgeTPU))
+
+
+if __name__ == '__main__':
+  main()
